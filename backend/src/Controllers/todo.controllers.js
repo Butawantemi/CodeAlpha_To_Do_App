b3e2d443680783b680c4e019a71e87d346ca6563
Butawantemi <japhetbuta@gmail.com>
2024-09-01 17:41:04 +0300

    adding createTodo controller

--- backend/src/Controllers/todo.controllers.js
+++ backend/src/Controllers/todo.controllers.js
@@ -1,3 +1,11 @@
 const Todo = require('../Models/todo.model')
 const asnycHandler = require('express-async-handler');
 
+// Create or add a new Todo.
+exports.createTodo = asnycHandler(async (req, res) => {
+    const todo = await Todo.create(req.body);
+    res.status(201).json(todo);
+})
+
+// G
+

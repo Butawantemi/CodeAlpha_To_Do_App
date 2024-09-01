a48c0b4aba5c8e71af708427d544b80c2f9f729d
Butawantemi <japhetbuta@gmail.com>
2024-09-01 18:27:13 +0300

    adding todo routes

--- backend/src/Routes/todo.routes.js
+++ backend/src/Routes/todo.routes.js
@@ -6,4 +6,16 @@ const todoController = require('../Controllers/todo.controllers');
 // Create a new Todo.
 router.post('/', todoController.createTodo);
 
-//
+//Get all Todos.
+router.get('/', todoController.getTodos);
+
+// Get a single Todo.
+router.get('/:id', todoController.getTodo);
+
+// Update a Todo.
+router.put('/:id', todoController.updateTodo);
+
+// Delete a Todo.
+router.delete('/id', todoController.deleteTodo);
+
+module.e

97fe494a1c78a258081733c39d62171267916a8b
Butawantemi <japhetbuta@gmail.com>
2024-09-01 14:44:03 +0300

    adding environment variable and configure

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
+const { config } = require('dotenv');
 const express = require('express');
+config()
 
 const app = express();
+port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 })
